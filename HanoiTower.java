a868a7a2f2344cb6a2b304ea633f3c7f7a2a6e1c
aafnx <aafnx@proton.me>
2022-10-23 14:54:52 +0300

    Выполнил задание Ханойская Башня

--- HanoiTower.java
+++ HanoiTower.java
@@ -1,5 +1,16 @@
+// Написать программу, показывающую последовательность действий для игры “Ханойская башня”
 public class HanoiTower {
   public static void main(String[] args) {
-    
+    start(4, 1, 3);
+  }
+  static void start(int lengthTower, int fromPin, int toPin) {
+    if (lengthTower == 1) {
+      System.out.printf("Переместить диск 1 со стержня - %d, на стержень - %d\n", fromPin, toPin);
+    } else {
+      int tmpPin = 6 - fromPin - toPin;
+      start(lengthTower - 1, fromPin, tmpPin);
+      System.out.printf("Переместить диск %d со стержня - %d, на стержень - %d\n", lengthTower, fromPin, toPin);
+      start(lengthTower - 1, tmpPin, toPin);
+    }
   }
 }
